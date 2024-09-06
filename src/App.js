58980a0d4f3051fd0d94458d701970e540c43185
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-06 12:59:39 +0300

    feat: add Item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App(){
   return(
     <div className="App">
@@ -26,16 +31,24 @@ function Form()
   )
 }
 
+function Item(){
+
+}
+
 function PackingList()
 {
   return (
-    <div className="list"> LIST</div>
+    // <div className="list"> LIST</div>
+    <ul className="list">
+      {initialItems.map(item =><Item item={item}/>)}
+
+    </ul>
   )
 }
 
 function Stats()
 {
-  return <footer>
+  return <footer class="stats">
     <em>
     👜 You have x items on your list , and you already packed x (x%)
     </em>
