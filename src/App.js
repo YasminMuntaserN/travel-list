63e97ff3eef07f029e070350a78ce5931c536c41
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-07 15:48:46 +0300

    move Form Component Into Separate File

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Logo } from "./components/Logo.js";
+import { Form } from "./components/Form.js";
+
 
 
 export default function App() {
@@ -45,51 +47,6 @@ export default function App() {
       />
       <Stats items={items}  />
     </div>
-  );
-}
-
-function Form({ onAddItem }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  // Handle form submission
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!description) return;
-
-    const newItem = {
-      description,
-      quantity,
-      packed: false,
-      id: Date.now(),
-    };
-
-    onAddItem(newItem);
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip?</h3>
-      <select
-        value={quantity}
-        onChange={e => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => 1 + i).map(num => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={e => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+export function Form({ onAddItem }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  // Handle form submission
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    onAddItem(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => 1 + i).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
